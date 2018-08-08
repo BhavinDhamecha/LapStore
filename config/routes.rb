658a800d98d7012ec67982841a8734a22b383a00
BhavinDhamecha <bhavin.dhamecha@softices.com>
2018-08-08 15:24:58 +0530

    Activity #4213- add custom path for devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users	
+  devise_for :users, skip: [:sessions, :registrations, :passwords]
+
+	devise_scope :user do
+    get 		'/signin', 					to: 'devise/sessions#new', 					as: 'new_user_session'
+    post 		'/signin', 					to: 'devise/sessions#create', 			as: 'user_session'
+
+    delete 	'/signout', 				to: 'devise/sessions#destroy', 			as: 'destroy_user_session'
+    
+    get 		'/signup', 					to: 'devise/registrations#new',			as: 'new_user_registration'
+    post		'/signup',					to: 'devise/registrations#create', 	as: 'user_registration' 	
+    put			'/signup',					to: 'devise/registrations#update', 	as: 'registration_path'
+    delete	'/signup',					to: 'devise/registrations#destroy', as: 'destroy_user'
+    get			'/edit-user',				to: 'devise/registrations#edit',		as: 'edit_user_registration'
+    
+    get			'/forgot-password',	to: 'devise/passwords#new', 				as: 'new_user_password'
+    post		'/forgot-password',	to: 'devise/passwords#create',			as: 'user_password'
+    get			'/edit-password', 	to: 'devise/passwords#edit',				as: 'edit_user_password'
+    put			'/forgot-password', to: 'devise/passwords#update',			as: 'password_path'
+  end
 
 	root to: 'laptops#index'
 
