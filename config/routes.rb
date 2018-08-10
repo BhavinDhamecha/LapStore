08010117039281958dcac9d674f6bda72ba087d9
BhavinDhamecha <bhavin.dhamecha@softices.com>
2018-08-10 16:53:20 +0530

    Activity #4228- added some custom routes using collection and member.

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions, :registrations, :passwords]
 
-	devise_scope :user do
-    get 		'/signin', 					to: 'devise/sessions#new', 					as: 'new_user_session'
-    post 		'/signin', 					to: 'devise/sessions#create', 			as: 'user_session'
-
-    delete 	'/signout', 				to: 'devise/sessions#destroy', 			as: 'destroy_user_session'
-    
-    get 		'/signup', 					to: 'devise/registrations#new',			as: 'new_user_registration'
-    post		'/signup',					to: 'devise/registrations#create', 	as: 'user_registration' 	
-    put			'/signup',					to: 'devise/registrations#update', 	as: 'registration_path'
-    delete	'/signup',					to: 'devise/registrations#destroy', as: 'destroy_user'
-    get			'/edit-user',				to: 'devise/registrations#edit',		as: 'edit_user_registration'
-    
-    get			'/forgot-password',	to: 'devise/passwords#new', 				as: 'new_user_password'
-    post		'/forgot-password',	to: 'devise/passwords#create',			as: 'user_password'
-    get			'/edit-password', 	to: 'devise/passwords#edit',				as: 'edit_user_password'
-    put			'/forgot-password', to: 'devise/passwords#update',			as: 'password_path'
-  end
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
 	root to: 'laptops#index'
 
-  resources :laptops
-  resources :categories
+  resources :laptops do
+    get 'edit', on: :member
+  end
+  resources :categories do
+    get 'index', on: :collection
+  end
   resources :payments
   resources :orders
   resources :customers
