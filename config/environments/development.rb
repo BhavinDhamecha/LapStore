c618c294ee62ea5d5031d06923e641d6375b6d96
BhavinDhamecha <bhavin.dhamecha@softices.com>
2018-08-11 11:31:46 +0530

    Activity #4231- Send reset password instruction via Gmail.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,9 +20,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
+    config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{2.days.to_i}" }
   else
     config.action_controller.perform_caching = false
 
@@ -33,7 +31,20 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    authentication:       "plain",
+    enable_starttls_auto: true,
+    user_name:            "dhamechabhavin23@gmail.com",
+    password:             "dhamecha769893"
+  }
 
   config.action_mailer.perform_caching = false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_for :users, skip: [:sessions, :registrations, :passwords]
+
+  devise_scope :user do
+    get     '/signin',          to: 'devise/sessions#new',          as: :new_user_session
+    post    '/signin',          to: 'devise/sessions#create',       as: 'user_session'
+
+    delete  '/signout',         to: 'devise/sessions#destroy',      as: 'destroy_user_session'
+    
+    get     '/signup',          to: 'devise/registrations#new',     as: 'new_user_registration'
+    post    '/signup',          to: 'devise/registrations#create',  as: 'user_registration'   
+    put     '/signup',          to: 'devise/registrations#update',  as: 'registration_path'
+    delete  '/signup',          to: 'devise/registrations#destroy', as: 'destroy_user'
+    get     '/edit-user',       to: 'devise/registrations#edit',    as: 'edit_user_registration'
+    
+    get     '/forgot-password', to: 'devise/passwords#new',         as: 'new_user_password'
+    post    '/forgot-password', to: 'devise/passwords#create',      as: 'user_password'
+    get     '/edit-password',   to: 'devise/passwords#edit',        as: 'edit_user_password'
+    put     '/forgot-password', to: 'devise/passwords#update',      as: 'password_path'
+  end
 
 	root to: 'laptops#index'
 
@@ -8,7 +26,7 @@ Rails.application.routes.draw do
     get 'edit', on: :member
   end
   resources :categories do
-    get 'index', on: :collection
+    get :index, on: :collection
   end
   resources :payments
   resources :orders
