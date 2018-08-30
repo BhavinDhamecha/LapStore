5faab03e923be9cd06717282d46939a15ad5b201
BhavinDhamecha <bhavin.dhamecha@softices.com>
2018-08-30 15:26:16 +0530

    Added view and controller for Terms & Condition.

--- config/routes.rb
+++ config/routes.rb
@@ -42,5 +42,7 @@ Rails.application.routes.draw do
   resources :brands
   resources :shops
 
+  get 'static_pages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
